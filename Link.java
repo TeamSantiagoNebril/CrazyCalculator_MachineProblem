383e66c9fab6e64b80ac350dc329df3a0196dcce
Beeyeah <santiagobea79@yahoo.com.ph>
2017-03-13 16:25:46 +0800

    Add files via upload

---
+++ Link.java
@@ -0,0 +1,13 @@
+
+public class Link {
+	private char element;
+	public Link next;
+
+	public char getElement(){
+		return element;
+	}
+	
+	public void setElement(char element){
+		this.element = element;
+	}
+}
