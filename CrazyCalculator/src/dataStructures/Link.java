04563bfc5ccd3f2a1dea178dac62f287c7e56e71
Unknown <aerolnebril@gmail.com>
2017-03-18 18:58:43 +0800

    Created Project file for integration with eclipse
    In order to have ease in editing and creating files to be uploaded to gihub repository, I transferred all the codes in  project folder so that we can easily edit this in Eclipse.
    
    P.S. I transferred the data structure classes to a 'dataStructure' package for proper encapsulation of said classes.

---
+++ CrazyCalculator/src/dataStructures/Link.java
@@ -0,0 +1,15 @@
+package dataStructures;
+
+
+public class Link {
+	private char element;
+	public Link next;
+
+	public char getElement(){
+		return element;
+	}
+	
+	public void setElement(char element){
+		this.element = element;
+	}
+}
---
+++ CrazyCalculator/src/dataStructures/PseudoArray.java
@@ -0,0 +1,20 @@
+package dataStructures;
+
+
+public class PseudoArray {
+	LinkedList list;
+	public PseudoArray(int size){
+		list = new LinkedList();
+		for(int a = 0; a < size; a++){
+			list.addElement();
+		}
+	}
+	
+	public void set(int index, char element){
+		list.setSpecificElement(index, element);
+	}
+	
+	public char get(int index){
+		return list.getElement(index);
+	}
+}
