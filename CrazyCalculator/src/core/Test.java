8081640e07a78ba849411420a32a7de15c49fd29
Unknown <aerolnebril@gmail.com>
2017-03-23 01:03:22 +0800

    Test class for Calculator class

---
+++ CrazyCalculator/src/core/Test.java
@@ -0,0 +1,14 @@
+package core;
+
+public class Test 
+{
+
+	
+	
+	public static void main(String args[])
+	{
+		Calculator C = new Calculator();
+		String input = "1+3/7";
+		System.out.println(C.translateInfixToPostfix(input));
+	}
+}
